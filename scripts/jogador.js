53f3840ba86186042b9d0bf886c4f2452dee70c4
Telmo <telmoregalado@gmail.com>
2025-05-13 09:27:37 +0100

    addedfetch player by id, updated version of db.json

--- scripts/jogador.js
+++ scripts/jogador.js
@@ -12,14 +12,79 @@ function fetchJogadorDetalhes() {
         .then(j => {
             const container = document.getElementById("detalhesJogador");
             container.innerHTML = `
-                <div class="text-center">
-                    <img src="${j.imagem}" class="img-fluid mb-3" style="max-width: 300px;">
-                    <h1>${j.nome}</h1>
-                    <p>Número: ${j.numero}</p>
-                    <p>Posição: ${j.posição}</p>
-                    <p>Idade: ${j.idade}</p>
-                    <span class="fi fi-${j.codigo} me-2"></span> ${j.nacionalidade}
+                <div class="row g-4 d-flex justify-content-center">
+            <div class="col-12 col-lg-4">
+                <div class="card mb-4">
+                    <div class="ratio ratio-1x1">
+                        <img src="${j.imagemPerfil}" class="img-fluid object-fit-cover" alt="${j.nome}" style="object-position: top;">
+                    </div>
+                    <div class="card-body">
+                    <div class="d-flex justify-content-between text-center">
+                        <h1 class="fs-3 fw-bold">
+                            ${j.nome}
+                        </h1>
+                        <h1 class="fs-3 fw-bold">${j.numero}</h1>
+                        </div>
+                        <div class="text-secondary">
+                            <span>${j.posição}</span>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-6 d-flex align-items-baseline">
+                                <p class="me-2 fs-6 fw-light">Idade:</p>
+                                <p class="fs-5 fw-medium">${j.idade}</p>
+                            </div>
+                            <div class="col-6 d-flex align-items-baseline">
+                                <p class="me-2 fs-6 fw-light">Altura:</p>
+                                <p class="fs-5 fw-medium">${j.altura}</p>
+                            </div>
+                            <div class="col-6 mt-3 d-flex align-items-baseline">
+                                <p class="me-2 fs-6 fw-light">Peso:</p>
+                                <p class="fs-5 fw-medium">${j.peso}</p>
+                            </div>
+                            <div class="col-6 mt-3"><span class="fi fi-${j.codigo} me-2"></span> ${j.nacionalidade}</div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+
+            <div class="col-12 col-lg-4">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Estatísticas de Carreira</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-muted">teste</p>
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Linha Temporal da Carreira</h5>
+                    </div>
+                    <div class="card-body timeline">
+                        <p class="text-muted">teste</p>
+                    </div>
+                </div>
+                
+                 <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Conquistas</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-muted">teste</p>
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Biografia</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-muted lh-base fs-6">${j.biografia}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
             `;
         })
         .catch(err => {
