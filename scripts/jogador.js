d307fe29f3de7ab2b541c942b46b33d954bdd05a
Telmo1337 <telmoregalado@gmail.com>
2025-06-10 20:54:01 +0100

    db json atualizada e mudanças no perfil de cada jogador / e tecnico

--- scripts/jogador.js
+++ scripts/jogador.js
@@ -20,34 +20,36 @@ function fetchJogadorDetalhes() {
             container.innerHTML = `
                 <div class="row g-4 d-flex justify-content-center">
             <div class="col-12 col-lg-4">
-                <div class="card mb-4">
-                    <div class="ratio ratio-1x1">
-                        <img src="${j.imagemPerfil}" class="img-fluid object-fit-cover" alt="${j.nome}" style="object-position: top;">
-                    </div>
-                    <div class="card-body">
-                    <div class="d-flex justify-content-between text-center">
-                        <h1 class="fs-3 fw-bold">
-                            ${j.nome}
-                        </h1>
-                        <h1 class="fs-3 fw-bold">${j.numero}</h1>
+                <div class="position-sticky" style="top: 1em;">
+                    <div class="card mb-4">
+                        <div class="ratio ratio-1x1">
+                            <img src="${j.imagemPerfil}" class="img-fluid object-fit-cover" alt="${j.nome}" style="object-position: top;">
                         </div>
-                        <div class="text-secondary">
-                            <span>${j.posição}</span>
-                        </div>
-                        <div class="row mt-4">
-                            <div class="col-6 d-flex align-items-baseline">
-                                <p class="me-2 fs-6 fw-light">Idade:</p>
-                                <p class="fs-5 fw-medium">${j.idade}</p>
+                        <div class="card-body">
+                        <div class="d-flex justify-content-between text-center">
+                            <h1 class="fs-3 fw-bold">
+                                ${j.nome}
+                            </h1>
+                            <h1 class="fs-3 fw-bold">${j.numero}</h1>
                             </div>
-                            <div class="col-6 d-flex align-items-baseline">
-                                <p class="me-2 fs-6 fw-light">Altura:</p>
-                                <p class="fs-5 fw-medium">${j.altura}</p>
+                            <div class="text-secondary">
+                                <span>${j.posição}</span>
                             </div>
-                            <div class="col-6 mt-3 d-flex align-items-baseline">
-                                <p class="me-2 fs-6 fw-light">Peso:</p>
-                                <p class="fs-5 fw-medium">${j.peso}</p>
+                            <div class="row mt-4">
+                                <div class="col-6 d-flex align-items-baseline">
+                                    <p class="me-2 fs-6 fw-light">Idade:</p>
+                                    <p class="fs-5 fw-medium">${j.idade}</p>
+                                </div>
+                                <div class="col-6 d-flex align-items-baseline">
+                                    <p class="me-2 fs-6 fw-light">Altura:</p>
+                                    <p class="fs-5 fw-medium">${j.altura}</p>
+                                </div>
+                                <div class="col-6 mt-3 d-flex align-items-baseline">
+                                    <p class="me-2 fs-6 fw-light">Peso:</p>
+                                    <p class="fs-5 fw-medium">${j.peso}</p>
+                                </div>
+                                <div class="col-6 mt-3"><span class="fi fi-${j.codigo} me-2"></span> ${j.nacionalidade}</div>
                             </div>
-                            <div class="col-6 mt-3"><span class="fi fi-${j.codigo} me-2"></span> ${j.nacionalidade}</div>
                         </div>
                     </div>
                 </div>
